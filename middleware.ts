297750afd2278d44337400bc2058ccd445cdacee
abubakerk <145732307+abubakerk@users.noreply.github.com>
2026-03-15 07:12:33 -0400

    middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,3 @@
-import { NextResponse, type NextRequest } from 'next/server'
-
-export async function middleware(request: NextRequest) {
-  return NextResponse.next()
-}
-
-export const config = {
-  matcher: ['/((?!_next/static|_next/image|favicon.ico).*)', ],
-}
+import { NextResponse } from 'next/server'
+export function middleware() { return NextResponse.next() }
+export const config = { matcher: [] }
